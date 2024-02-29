92edca24980343b4e453d40a1e19893d0eda3e1d
YURII D <gkhelloworld@gmail.com>
2024-02-29 11:54:08 +0200

    up

--- src/routes/service-worker.ts
+++ src/routes/service-worker.ts
@@ -8,8 +8,5 @@
  * You can also use this file to add more functionality that runs in the service worker.
  */
 import { setupServiceWorker } from "@builder.io/qwik-city/service-worker";
-import { setupPwa } from "@qwikdev/pwa/sw";
 
 setupServiceWorker();
-
-setupPwa();
--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,10 @@ import { defineConfig, type UserConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { type PWAOptions, qwikPwa } from "@qwikdev/pwa";
-
-const config: PWAOptions | undefined = process.env.CUSTOM_CONFIG === "true"
- ? { config: true }
-    : undefined;
 
 export default defineConfig((): UserConfig => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths(),
-      qwikPwa(config)],
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
     define: {
       // (optional) enables debugging in workbox
       "process.env.NODE_ENV": JSON.stringify("development"),

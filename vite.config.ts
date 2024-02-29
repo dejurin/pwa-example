842bc9909c348fa7f329c7b4a5abbcf510c8f1fc
YURII D <gkhelloworld@gmail.com>
2024-02-29 11:44:28 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig, type UserConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
-import { type PWAOptions, qwikPwa } from "@qwikdev/pwa";
-import deadFile from "vite-plugin-deadfile";
 import tsconfigPaths from "vite-tsconfig-paths";
-import rehypeExternalLinks from 'rehype-external-links';
+import { type PWAOptions, qwikPwa } from "@qwikdev/pwa";
 
 const config: PWAOptions | undefined = process.env.CUSTOM_CONFIG === "true"
  ? { config: true }
@@ -12,40 +10,13 @@ const config: PWAOptions | undefined = process.env.CUSTOM_CONFIG === "true"
 
 export default defineConfig((): UserConfig => {
   return {
-    build: {
-      chunkSizeWarningLimit: 500,
-    },
-    esbuild: {
-      drop: ['console', 'debugger'],
-    },
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths(),
+      qwikPwa(config)],
     define: {
       // (optional) enables debugging in workbox
       "process.env.NODE_ENV": JSON.stringify("development"),
     },
-    plugins: [
-      qwikCity({
-        mdxPlugins: {
-          remarkGfm: false,
-          rehypeSyntaxHighlight: false,
-          rehypeAutolinkHeadings: false,
-        },
-        mdx: {
-          rehypePlugins: [
-            [rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer', 'nofollow'] }],
-          ],
-        },
-      }),
-      qwikVite(),
-      tsconfigPaths(),
-      qwikPwa(config),
-      deadFile({
-        root: "src",
-      }),
-    ],
     server: {
-      strictPort: true,
-      // host: "app.local",
-      port: 5173,
       headers: {
         "Cache-Control": "public, max-age=0",
       },

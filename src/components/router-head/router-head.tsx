a28676472f3bae739de8942561615a0f0c3847ad
YURII D <gkhelloworld@gmail.com>
2024-02-29 11:58:09 +0200

    add pwa

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -1,5 +1,6 @@
 import { component$ } from "@builder.io/qwik";
 import { useDocumentHead, useLocation } from "@builder.io/qwik-city";
+import * as pwaHead from "@qwikdev/pwa/head";
 
 /**
  * The RouterHead component is placed inside of the document `<head>` element.
@@ -21,6 +22,13 @@ export const RouterHead = component$(() => {
       ))}
 
       {head.links.map((l) => (
+        <link key={l.key} {...l} />
+      ))}
+
+      {pwaHead.meta.map((l) => (
+        <meta key={l.key} {...l} />
+      ))}
+      {pwaHead.links.map((l) => (
         <link key={l.key} {...l} />
       ))}
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,25 @@
-import { defineConfig, type UserConfig } from "vite";
+import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { type PWAOptions, qwikPwa } from "@qwikdev/pwa";
 
-export default defineConfig((): UserConfig => {
+const config: PWAOptions | undefined = process.env.CUSTOM_CONFIG === "true"
+ ? { config: true }
+    : undefined;
+
+export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
     define: {
-      // (optional) enables debugging in workbox
+      // enables debugging in workbox
       "process.env.NODE_ENV": JSON.stringify("development"),
     },
-    server: {
-      headers: {
-        "Cache-Control": "public, max-age=0",
-      },
-    },
+    plugins: [
+      qwikCity(),
+      qwikVite(),
+      tsconfigPaths(),
+      qwikPwa(config)
+    ],
     preview: {
       headers: {
         "Cache-Control": "public, max-age=600",

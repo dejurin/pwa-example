15bd793dc274df3d54a2c62031e7b9343a743b56
YURII D <gkhelloworld@gmail.com>
2024-02-29 11:32:23 +0200

    upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,20 @@ import { defineConfig, type UserConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { type PWAOptions, qwikPwa } from "@qwikdev/pwa";
+
+const config: PWAOptions | undefined = process.env.CUSTOM_CONFIG === "true"
+ ? { config: true }
+    : undefined;
 
 export default defineConfig((): UserConfig => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths(),
+      qwikPwa(config)],
+    define: {
+      // (optional) enables debugging in workbox
+      "process.env.NODE_ENV": JSON.stringify("development"),
+    },
     server: {
       headers: {
         "Cache-Control": "public, max-age=0",
